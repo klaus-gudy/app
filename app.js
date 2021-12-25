a8709e19e6eb4cc01fb5a45d0b8b41e3a0c841eb
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2021-12-25 14:13:52 +0300

    made appendings using java script

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
-const container_div = document.getElementsByClassName('container');
+const container_div = document.getElementById('cont');
 
 const h1_header = document.createElement('h1');
 h1_header.className = 'p-4';
 h1_header.innerText = 'javascript app';
 
+container_div.append(h1_header);
+
 const hr = document.createElement('hr');
+container_div.append(hr);
 
 const card_div = document.createElement('div');
 card_div.className = 'card';
+container_div.append(card_div);
 
 const h2 = document.createElement('h2');
 h2.className = 'py-2';
+h2.innerText = 'add a book';
+card_div.appendChild(h2);
 
 const form = document.createElement('form');
 form.className = 'd-flex justify-content-xl-center';
+card_div.appendChild(form);
 
 const form_div = document.createElement('div');
 form_div.className = 'form-group p-2';
+form.appendChild(form_div);
 
 const text_input = document.createElement('input');
 text_input.className = 'val';
 text_input.setAttribute('type','text');
 text_input.setAttribute('placeholder', 'enter a book name');
+form_div.appendChild(text_input);
 
 const button = document.createElement('button');
 button.className = 'btn btn-primary';
 button.setAttribute('id', 'submit');
 button.setAttribute('type', 'submit');
+button.innerText = 'submit';
+form_div.appendChild(button);
 
 const unli = document.createElement('ul');
 unli.className = 'list-group';
+card_div.appendChild(unli);
 
 const li = document.createElement('li');
-li.className = 'list-group-item d-flex justify-content-between align-items-center'
+li.className = 'list-group-item d-flex justify-content-between align-items-center';
+li.innerText = 'list';
+unli.appendChild(li);
 
 const span = document.createElement('span');
 span.innerHTML = '<i class="fas fa-trash"></i>';
-
-// appendings
-li.appendChild(span)
-
-container_div.append(h1_header);
+li.appendChild(span);
